5ed007abdd806be11d85e6d8cc71053147a919ee
longpaladin <30802551+longpaladin@users.noreply.github.com>
2021-01-10 02:22:46 +0800

    Add files via upload

---
+++ 4Dgenerator.py
@@ -0,0 +1,4 @@
+import random
+
+new4Dnumber = random.randint(0,9999)
+print('The 4D number you should buy is: %04d' % (new4Dnumber))
---
+++ TOTOgenerator.py
@@ -0,0 +1,5 @@
+import random
+
+L = random.sample(range(1,50),6)
+L = sorted(L)
+print(L)
