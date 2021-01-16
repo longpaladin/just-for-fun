2b8c41b4beebceb234f5da69622f152b1528d5d1
longpaladin <30802551+longpaladin@users.noreply.github.com>
2021-01-16 17:14:21 +0800

    Update 4Dgenerator.py

--- 4Dgenerator.py
+++ 4Dgenerator.py
@@ -6,4 +6,12 @@ Last updated: 9 Jan 2021
 import random
 
 new4Dnumber = random.randint(0,9999)
+
+# Modulo string formatting
 print('The 4D number you should buy is: %04d' % (new4Dnumber))
+
+# Format method string formatting
+print('The 4D number you should buy is: {:04d}'.format(new4Dnumber))
+
+# F-string formatting
+print(f'The 4D number you should buy is: {new4Dnumber:04d}')
