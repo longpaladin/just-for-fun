674e8dc710344e21494a2799fb1e82506cd2bc72
longpaladin <30802551+longpaladin@users.noreply.github.com>
2021-01-18 16:40:43 +0800

    Add files via upload

---
+++ stringformatting.py
@@ -0,0 +1,10 @@
+base = 8
+height = 8
+areaOfTriangle = base*height*0.5
+print(str(areaOfTriangle) + " cm^2")
+
+import math
+
+radius = 5
+areaOfCircle = radius**2*math.pi
+print(str(areaOfCircle) + " cm\u00b2")
